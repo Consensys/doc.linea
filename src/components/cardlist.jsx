82077b4d3a5f9ee6eb6ea67488816fb0902a7d47
cedrabner <134417957+cedrabner@users.noreply.github.com>
2023-12-01 10:04:01 -0500

    better sidebar logic (#365)
    created better sidebar logic and auto generated doc cards

--- src/components/cardlist.jsx
+++ src/components/cardlist.jsx
@@ -1,38 +1,30 @@
 import React, { useState } from "react";
+import Link from '@docusaurus/Link'; // Correct import
 
 function Card({ title, items }) {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div
-      style={{
-        border: "2px solid #ddd",
-        borderRadius: "10px",
-        margin: "1em",
-        padding: "1em",
-        transition: "0.3s",
-        boxShadow: isHovered ? 
-        "0 8px 16px 0 rgba(0,0,0,0.2)" : "0 4px 8px 0 rgba(0,0,0,0.2)",
-        height: "200px",
-        width: "200px",
-        overflow: "scroll",
-      }}
-      onMouseOver={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <h3 style={{ fontWeight: isHovered ? "bold" : "normal" }}>{title}</h3>
-        {isHovered && (
-        <ul>
-        {items.map((item, index) => (
-            <li key={index}>
-               <a href={item.link} style={{ fontWeight: "normal" }}>
-              {item.label}
-              </a>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+    <Link to={items[0].link} style={{ textDecoration: 'none', color: 'inherit' }}>
+      <div
+        style={{
+          border: "2px solid #ddd",
+          borderRadius: "10px",
+          margin: "1em",
+          padding: "1em",
+          transition: "0.3s",
+          boxShadow: isHovered ? 
+          "0 8px 16px 0 rgba(0,0,0,0.2)" : "0 4px 8px 0 rgba(0,0,0,0.2)",
+          height: "200px",
+          width: "200px",
+          overflow: "scroll",
+        }}
+        onMouseOver={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <h3 style={{ fontWeight: isHovered ? "bold" : "normal" }}>{title}</h3>
+      </div>
+    </Link>
   );
 }
 
