ae6115a822475a07f3c9b83e75d14538fd6a5d95
jlwllmr <95916148+jlwllmr@users.noreply.github.com>
2024-03-04 12:00:40 +0000

    Fix sidebar configuration

--- sidebars.js
+++ sidebars.js
@@ -328,7 +328,7 @@ const sidebars = {
         label: "Sequencer",
         link: {
           type: "doc",
-          id: "architecture/evm-state-manager/index",
+          id: "architecture/sequencer/index",
         },
         items: [
           "architecture/sequencer/conflation",
