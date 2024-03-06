b3d035a891e751ebed10a46abd4a52cd6ae8a81c
sammysango <136692631+sammysango@users.noreply.github.com>
2024-03-06 13:25:50 +0000

    adding social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -216,6 +216,11 @@ const config = {
           "python",
         ],
       },
+      metadata: [
+        {name: 'twitter:card', content: 'summary_large_image'},
+        {name: 'twitter:image', content: '/img/Linea_social_card_docs.png'},
+        {property: 'og:image', content: '/img/Linea_social_card_docs.png'},
+      ],
       languageTabs: [
         {
           highlight: "bash",
