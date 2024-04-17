221f0befae26c05e35f45da6e4a2fd44e4f160de
jlwllmr <95916148+jlwllmr@users.noreply.github.com>
2024-04-17 16:30:33 +0100

    Add Onthis Shortcuts page (#537)
    * Add Onthis Shortcuts page
    
    * Add image, plus other adjustments
    
    * Update project-words.txt
    
    * Move fees section up
    
    * Alter wording for clarity

--- sidebars.js
+++ sidebars.js
@@ -255,6 +255,7 @@ const sidebars = {
           items: [
             "build-on-linea/tooling/cross-chain/axelar",
             "build-on-linea/tooling/cross-chain/layerzero",
+            "build-on-linea/tooling/cross-chain/shortcuts",
           ],
         },
         {
