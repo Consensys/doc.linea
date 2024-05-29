16a21076e95387afd0356f9db975b4027f2f741c
jlwllmr <95916148+jlwllmr@users.noreply.github.com>
2024-05-29 10:33:23 +0100

    Adjust sidebar again

--- sidebars.js
+++ sidebars.js
@@ -288,9 +288,9 @@ const sidebars = {
           },
           items: [
             "developers/tooling/data-indexers/arkham",
+            "developers/tooling/data-indexers/covalent",
             "developers/tooling/data-indexers/decommas",
             "developers/tooling/data-indexers/dune",
-            "developers/tooling/data-indexers/covalent",
             "developers/tooling/data-indexers/envio",
             "developers/tooling/data-indexers/flair",
             {
