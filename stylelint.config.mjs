217a6d5933d7cb283baf2b3ed6d3fde0df23f250
Joel Willmore <95916148+jlwllmr@users.noreply.github.com>
2024-08-29 09:18:29 +0100

    Bump dependencies and edit linter config formats (#708)
    * Round 1
    
    * Round 2
    
    * Round 3
    
    * Various
    
    * Various
    
    * Update .nvmrc
    
    * Add sharp packages.
    
    Signed-off-by: bgravenorst <byron.gravenorst@consensys.net>
    
    * Update node versions.
    
    Signed-off-by: bgravenorst <byron.gravenorst@consensys.net>
    
    * Update node versions.
    
    Signed-off-by: bgravenorst <byron.gravenorst@consensys.net>
    
    ---------
    
    Signed-off-by: bgravenorst <byron.gravenorst@consensys.net>
    Co-authored-by: bgravenorst <byron.gravenorst@consensys.net>

---
+++ stylelint.config.mjs
@@ -0,0 +1,17 @@
+export default {
+    extends: ["stylelint-config-standard"],
+    rules: {
+      "selector-pseudo-class-no-unknown": [
+        true,
+        {
+          ignorePseudoClasses: ["global"],
+        },
+      ],
+      "selector-class-pattern": null,
+      "custom-property-empty-line-before": null,
+      "selector-id-pattern": null,
+      "declaration-empty-line-before": null,
+      "comment-empty-line-before": null,
+      "value-keyword-case": ["lower", { camelCaseSvgKeywords: true }],
+    },
+  };
