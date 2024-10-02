81185ea024147dc08b238e53795aea46e8d7b35a
jlwllmr <95916148+jlwllmr@users.noreply.github.com>
2024-10-02 07:48:09 +0100

    First iteration

---
+++ src/theme/DocItem/Layout/getfeedback.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect, useRef } from "react";
+import { useColorMode } from "@docusaurus/theme-common";
+
+const GetFeedback = (props) => {
+  const feedbackContRef = useRef();
+  const { colorMode } = useColorMode();
+  const [theme, setTheme] = useState();
+
+  useEffect(() => {
+    setTheme(colorMode);
+  }, [colorMode]);
+
+  useEffect(() => {
+    setTheme(window?.localStorage?.getItem("theme") || colorMode);
+    window.usabilla.load("w.usabilla.com", "3ab372ce5993");
+  }, []);
+
+  return (
+    <div className="getfeedback-container">
+      {/*Light*/}
+      <div
+        ub-in-page="66fbd0c790845616a838f8a9"
+        className={theme === "dark" ? "getfeedback-hidden" : undefined}
+        ref={feedbackContRef}
+      />
+      {/*Dark*/}
+      <div
+        ub-in-page="66fbd0c790845616a838f8a9"
+        className={theme === "light" ? "getfeedback-hidden" : undefined}
+        ref={feedbackContRef}
+      />
+    </div>
+  );
+};
+
+export default GetFeedback;
