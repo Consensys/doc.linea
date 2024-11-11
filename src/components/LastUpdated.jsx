3c20d3922da9bce0cf51faddeff82c152c4389aa
jlwllmr <95916148+jlwllmr@users.noreply.github.com>
2024-11-11 10:14:09 +0000

    Remove comments

--- src/components/LastUpdated.jsx
+++ src/components/LastUpdated.jsx
@@ -9,10 +9,6 @@ const LastUpdated = () => {
       year: 'numeric',
       month: 'short',
       day: 'numeric',
-      // hour: '2-digit',
-      // minute: '2-digit',
-      // second: '2-digit',
-      // hour12: false
     });
   };
 
