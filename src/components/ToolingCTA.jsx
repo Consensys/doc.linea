1f543e10682502a0576732e0d36ac7ce508b1376
jlwllmr <95916148+jlwllmr@users.noreply.github.com>
2024-12-03 09:18:27 +0000

    Edit tooling component

--- src/components/ToolingCTA.jsx
+++ src/components/ToolingCTA.jsx
@@ -13,14 +13,8 @@ export default function ToolingCTA() {
   return (
     <div style={{ marginTop: "2rem" }}>
       <Admonition type="info" title="Want to be featured here?">
-        <p>
           Have you built a tool for Linea? Contribute to the docs to add it
-          here! See our{" "}
-          <a href="https://github.com/Consensys/doc.linea?tab=readme-ov-file#contribute">
-            guide to contributing
-          </a>
-          .
-        </p>
+          here! See our <a href="https://github.com/Consensys/doc.linea?tab=readme-ov-file#contribute"> guide to contributing</a>.
       </Admonition>
     </div>
   );
