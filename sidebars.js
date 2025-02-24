5fab3b7251c5002ac2222895394c2598a049de1e
Nenad Vitorovic <nvitorovic@gmail.com>
2025-02-24 15:38:43 +0100

    Add info on Tenderly

--- sidebars.js
+++ sidebars.js
@@ -66,6 +66,7 @@ const sidebars = {
             "get-started/how-to/verify-smart-contract/atlas",
             "get-started/how-to/verify-smart-contract/foundry",
             "get-started/how-to/verify-smart-contract/hardhat",
+            "get-started/how-to/verify-smart-contract/tenderly",
           ],
         },
         "get-started/how-to/migrate-dapp",
@@ -255,6 +256,7 @@ const sidebars = {
           },
           items: [
             "get-started/tooling/gas/blocknative",
+            "get-started/tooling/gas/tenderly",
           ],
         },
         {
@@ -347,6 +349,7 @@ const sidebars = {
             "get-started/tooling/security/hypernative",
             "get-started/tooling/security/scamfari",
             "get-started/tooling/security/spherex",
+            "get-started/tooling/security/tenderly",
           ],
         },
         {
