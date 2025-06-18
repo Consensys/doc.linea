de3204ccd1759ce6191b955302752a88ee1a54f4
jlwllmr <95916148+jlwllmr@users.noreply.github.com>
2025-06-18 12:06:18 +0100

    Remove star icon

--- src/theme/DocCardList/index.js
+++ src/theme/DocCardList/index.js
@@ -8,16 +8,6 @@ import DocCard from '@theme/DocCard';
 import styles from './styles.module.css';
 import featuredArticles from '../FeaturedArticles.json';
 
-const StarIcon = () => (
-  <svg className={styles.featuredIcon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 842.319 810.161">
-    <path
-      d="M508.066,63.21l51.462,158.383c3.437,10.576,13.292,17.737,24.413,17.737h166.533c88.618,0,125.463,113.399,53.77,165.487l-134.728,97.886c-8.997,6.537-12.762,18.123-9.325,28.699l51.462,158.383c27.384,84.28-69.078,154.365-140.772,102.277l-134.728-97.886c-8.997-6.537-21.18-6.537-30.176,0l-134.728,97.886c-71.693,52.088-168.156-17.996-140.772-102.277l51.462-158.383c3.437-10.576-.328-22.163-9.325-28.699l-134.728-97.886C-33.809,352.729,3.037,239.33,91.654,239.33h166.533c11.121,0,20.977-7.161,24.413-17.737l51.462-158.383c27.384-84.28,146.619-84.28,174.003,0Z"
-      fill="currentColor"
-      strokeWidth="0"
-    />
-  </svg>
-)
-
 function sortItems(items) {
   return items.sort((a, b) => {
     // Sort featured items first, then alphabetically by label
@@ -64,7 +54,7 @@ export default function DocCardList(props) {
       {featuredItems.length > 0 && (
         <>
           <h2 className={clsx(styles.sectionTitle, styles.featuredTitle)}>
-            Featured articles <StarIcon />
+            Featured articles
           </h2>
           <section className={clsx(styles.docCardListContainer, 'row', className)}>
             {featuredItems.map((item, index) => (
