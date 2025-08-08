52111d402e45c83f1420e879c22d20105d58dda8
jlwllmr <95916148+jlwllmr@users.noreply.github.com>
2025-08-08 16:44:07 +0100

    Add to sidebar

--- sidebars.js
+++ sidebars.js
@@ -85,6 +85,7 @@ const sidebars = {
             id: "get-started/how-to/run-a-node/index",
           },
           items: [
+            "get-started/how-to/run-a-node/beta-v4-migration",
             "get-started/how-to/run-a-node/besu",
             "get-started/how-to/run-a-node/erigon",
             "get-started/how-to/run-a-node/geth",
